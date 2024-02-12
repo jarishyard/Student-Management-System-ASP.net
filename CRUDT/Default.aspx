18490abbb02b36d138b624dc95af1074d867f8ad
91936 <91936@JARISH>
2024-02-13 00:07:51 +0530

    Add project files.

---
+++ CRUDT/Default.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CRUDT._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+   <div>
+       <div style="font-size:x-large" align="center"> Student Info Manage Form</div>
+       <br />
+
+       <table class="w-50">
+           <tr>
+               <td style="width: 287px; height: 13px;"></td>
+               <td style="width: 240px; height: 13px;">Student ID</td>
+               <td style="font-size: medium; height: 13px">
+                       <asp:TextBox ID="StudentID" runat="server" />
+               </td>
+           </tr>
+           <tr>
+               <td style="width: 287px">&nbsp;</td>
+               <td style="width: 240px">Student Name</td>
+               <td style="font-size: medium">
+                    <asp:TextBox ID="StudentName" runat="server" />
+
+               </td>
+           </tr>
+           <tr>
+               <td style="width: 287px">&nbsp;</td>
+               <td style="width: 240px">Country</td>
+               <td>
+                   <asp:DropDownList ID="Country" runat="server">
+                       <asp:ListItem>USA</asp:ListItem>
+                       <asp:ListItem>INDIA</asp:ListItem>
+                       <asp:ListItem>ENGLAND</asp:ListItem>
+                   </asp:DropDownList>
+               </td>
+           </tr>
+           <tr>
+               <td style="width: 287px">&nbsp;</td>
+               <td style="width: 240px">Age</td>
+               <td style="font-size: medium">
+                    <asp:TextBox ID="Age" runat="server" />
+
+               </td>
+           </tr>
+           <tr>
+               <td style="width: 287px">&nbsp;</td>
+               <td style="width: 240px">Mobile</td>
+               <td style="font-size: medium">
+                    <asp:TextBox ID="Mobile" runat="server" />
+                   </td>
+           </tr>
+           <tr>
+               <td style="width: 287px">&nbsp;</td>
+               <td style="width: 240px">&nbsp;</td>
+               <td>
+                   <asp:Button ID="Button1" runat="server" ForeColor="Black" OnClick="Button1_Click" Text="Insert" Width="93px" />
+                   <br />
+                   <br />
+                   
+               </td>
+           </tr>
+           <tr>
+               <td>
+                   <asp:GridView ID="StudentList" runat="server" />
+               </td>
+           </tr>
+       </table>
+
+       </div>
+
+</asp:Content>
---
+++ CRUDT/Default.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CRUDT
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                LoadStudents();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (StudentID.Text.Length == 0)
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Please Enter Student ID');", true);
+                return;
+            }
+
+            if (StudentName.Text.Length==0)
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Please Enter Student Name');", true);
+                return;
+            }
+
+            if (Country.SelectedIndex <= 0)
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Please Select Country');", true);
+                return;
+            }
+
+            if (Age.Text.Length == 0)
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Please Enter Age');", true);
+                return;
+            }
+
+            if (Mobile.Text.Length == 0)
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Please Enter Mobile');", true);
+                return;
+            }
+
+            SqlConnection con = new SqlConnection("Data Source = JARISH; Initial Catalog = ProgrammingDB; Persist Security Info = True; User ID = sa; Password = 1234567; ");
+            con.Open();
+            SqlCommand comm = new SqlCommand("Insert into Studentinfo_Tab values(" + int.Parse(StudentID.Text) + ", '" + StudentName.Text + "', '" + Country.SelectedValue + "', " + double.Parse(Age.Text) + ", '" + Mobile.Text + "')", con);
+            comm.ExecuteNonQuery();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Inserted');", true);
+            
+            con.Close();
+
+            LoadStudents();
+
+
+        }
+
+         void LoadStudents()
+        {
+            SqlConnection con = new SqlConnection("Data Source = JARISH; Initial Catalog = ProgrammingDB; Persist Security Info = True; User ID = sa; Password = 1234567; ");
+            con.Open();
+
+            SqlCommand comm = new SqlCommand("select * from Studentinfo_Tab;", con);
+
+            DataTable dtStudentList = new DataTable();
+            dtStudentList.Load(comm.ExecuteReader());
+            StudentList.DataSource = dtStudentList;
+            StudentList.DataBind();
+
+            con.Close();
+
+
+        }
+    }
+
+
+}
